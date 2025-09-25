9d4cf061080ff5d6abc984eb0327b6df13cd840f
Will.dev <wiiillian21@gmail.com>
2025-09-24 21:49:20 -0300

    Funções publicas e privadas

--- Aula16/math/math.go
+++ Aula16/math/math.go
@@ -4,6 +4,10 @@ func Sum(a, b int) int {
 	return a + b
 }
 
-func Sub(a, b int) int {
+func Sub(a, b int) int { // função publica
 	return a - b 
+}
+
+func multi(a, b int) int { // função privada
+	return a * b
 }
