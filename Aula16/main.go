47f37dcb96966f668c69e0f44e06f45207ff1acf
Will.dev <wiiillian21@gmail.com>
2025-09-24 21:32:34 -0300

    Pacotes externos, usando o go mod

--- Aula16/main.go
+++ Aula16/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"calculator/math"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// echo instace
+	e := echo.New()
+	e.Listener.Close()
+
 	sum := math.Sum(5, 6)
 	sub := math.Sub(10, 4)
 
